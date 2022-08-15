4552a9004e5337c8f45be180765eb5a373838c1b
Piyush Kumar <kpiyush39@gmail.com>
2022-08-15 05:36:06 +0530

    PORT SETTING CHANGED

--- app.js
+++ app.js
@@ -167,9 +167,17 @@ app.post("/delete", function(req, res) {
   }
 
 });
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Server running at port 3000");
 });
+
+//
+// let port = process.env.PORT;
+// if(port == null || port ==""){
+//   port =3000;
+// }
+//
+// app.listen(port, function() {
+//   console.log("Server started on port 3000");
+// });
